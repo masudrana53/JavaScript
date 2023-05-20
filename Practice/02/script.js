b763fbe05172d86f89503320e1147a2f7af1744d
Masud Rana <masudrana.20091@gmail.com>
2023-05-20 13:04:25 +0600

    updated

---
+++ Practice/02/script.js
@@ -0,0 +1,27 @@
+let x=60
+let y=50
+let result=x*y;
+console.log(result);
+
+let a=80
+let b=60
+let result1=a+b;
+console.log(result1);
+
+let c=500
+let d=200
+let result2=c-d;
+console.log(result2);
+
+d=250
+console.log(d);
+
+c=180
+console.log(c);
+
+
+
+let radius=5;
+const pi=8.1416;
+let areaOfCircle=pi*radius*radius;
+console.log(areaOfCircle);
---
+++ Practice/03/script.js
@@ -0,0 +1,22 @@
+let x=10
+console.log(x%=15)
+
+// Comparision Operator(<> == != >= <=) = result will be boolean (true or false)
+let a=21
+let b=20
+
+console.log(a<b);
+console.log(a>b);
+console.log(a<=b);
+console.log(a>=b);
+console.log(a==b);
+console.log(a!=b);
+
+
+
+
+// Logical Operator
+// and(&&) or(||) not(!)
+
+// username="java" && password="999" && (gender="male" || "female") && country="Bangladesh"
+
