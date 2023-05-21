cffcd853f3949854cf9c389700f137b27ebd8006
Masud Rana <masudrana.20091@gmail.com>
2023-05-21 13:10:24 +0600

    updated

---
+++ if statement/If statement.js
@@ -0,0 +1,59 @@
+// let x=10
+// let y=20
+
+// if(x==y){
+//     console.log("True");
+// }
+
+// else{
+//     console.log("False");
+// }
+
+// // // find odd or even numbenr:
+// let d=19
+
+// if(d%3==2){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+// // 0-18 cool, 19-27 normal, 28>= abnormal.
+// let weather=25;
+
+// if(weather<0){
+//     console.log("invalid weather");
+// }
+
+// else if(weather<=18){
+//     console.log("cool weather");
+// }  
+
+// else if(weather<=27){
+//     console.log("normal weather");
+// }  
+
+// else{
+//     console.log("abnormal weather");
+// }
+
+
+// 0-10 cool, 11-15 normal, 15>= abnormal.
+let weather=-3;
+
+if(weather>=15){
+ console.log("abnormal");
+}
+
+else if(weather>=11){
+    console.log("normal");
+}
+
+else if(weather>=0){
+    console.log("cool");
+}
+
+else {
+    console.log("minus");
+}
---
+++ loop/loop.js
@@ -0,0 +1,48 @@
+// for(let i=9; i<=100; i++){
+//     console.log(i);
+// }
+
+
+
+// for(let i=9; i<=100; i--){
+//     console.log(i);
+// }
+
+
+
+// for(let i=1; i<=100; i++){
+
+//     if(i%2!=0){
+//     console.log(i);
+//     }
+
+//     else{
+          
+//     }
+// }
+
+
+
+// i-50, divided by 3 or 5
+
+// for(let i=1; i<=100; i++){
+//     if(i%3==0 && i%5==00){
+//     console.log(i);
+//     }
+
+//     else{
+          
+//     }
+// }
+
+
+
+// Factorial of 5
+
+let input=5;
+let result=1;
+
+for(let i=input; i>0; i--){
+    result=result*i;
+}
+console.log(result);
