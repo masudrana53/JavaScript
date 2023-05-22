72652d136c2bb4178f0b3269acc46c975ab69ca4
Masud Rana <masudrana.20091@gmail.com>
2023-05-22 12:48:22 +0600

    updated loop

---
+++ loop/Switch.js
@@ -0,0 +1,29 @@
+// Swithc statement
+let day=6;
+
+switch(day){
+case 1:
+    console.log("Saturday");
+    break;
+case 2:
+    console.log("Sunday");
+    break;
+case 3:
+    console.log("Monday");
+    break;  
+case 4:
+    console.log("Tusday");
+    break;
+case 5:
+    console.log("Wednesday");
+    break;
+case 6:
+    console.log("Thursdayd");
+    break;
+case 7:
+    console.log("Friday")   
+    break;  
+
+    default:
+        console.log("Nothing found");
+}
---
+++ loop/loop 02.js
@@ -0,0 +1,120 @@
+// let userName="java";
+// let password="asd123";
+
+// if (userName=="java"){
+//     console.log("welcome" + userName);
+// }
+
+// else{
+//     console.log("password is not incorrect");
+
+// }
+
+
+
+
+// // Grade point 
+// let marks=105;
+
+// if(marks<0){
+//     console.log("Invalid Number");
+
+// }
+
+// else if(marks<=32){
+//     console.log("F");
+
+// }
+
+// else if(marks<=39){
+//     console.log("D");
+
+// }
+
+// else if(marks<=49){
+//     console.log("C");
+
+// }
+
+// else if(marks<=59){
+//     console.log("B");
+
+// }
+
+// else if(marks<=69){
+//     console.log("A-");
+
+// }
+
+// else if(marks<=79){
+//     console.log("A");
+
+// }
+
+// else if(marks<=100){
+//     console.log("A+");
+
+// }
+
+// else{
+//     console.log("Invalid Number");
+// }
+
+
+
+
+// // prime number
+
+// let number=2;
+// let counter=0;
+
+// for(let i=1; i<=number; i++){
+    
+//     if(number%i==0){
+//         counter=counter+1;
+//     }    
+    
+//     } 
+
+// if(counter==2){
+//     console.log("prime number");
+// }    
+    
+//     else{
+//         console.log ("not found");
+//     }
+    
+
+
+
+// swap or swaping
+
+// let x=10;
+// let y=20;
+// let temp=0;
+
+// temp=x;
+// x=y;
+// y=temp;
+
+// console.log("x=" +x+" y="+y)
+
+
+
+// Fibonnacci - 0 1 1 2 3 5 8 13
+
+let n1=0;
+let n2=1;
+let sum=0;
+console.log(n1+" \n" +n2);
+
+for (let i=3; i<=8; i++){
+    sum=n1+n2;
+    console.log(sum);
+    n1=n2;
+    n2=sum;
+
+}
+
+
+
